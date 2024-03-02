a359e33231d6adc8f722c19099410e7bd9e6e774
KangBaekGwa <ksu9801@gmail.com>
2024-03-02 22:05:30 +0900

    feat: Spring Jpa CURD  -

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -1,15 +1,23 @@
 package hello.itemservice.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login/loginForm";
     }
 }
