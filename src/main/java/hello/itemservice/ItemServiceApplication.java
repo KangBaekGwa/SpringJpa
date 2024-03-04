a7d284dfdc4154ce1380ada80892344845576cbb
KangBaekGwa <ksu9801@gmail.com>
2024-03-05 00:24:50 +0900

    feat: Spring Jpa CURD  -

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 
 
 //@Import(MemoryConfig.class)
